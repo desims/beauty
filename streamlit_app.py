670b6b9dd24d3838fcfbb00135118edde100977e
Desi Sitompul <sdesimayasari@gmail.com>
2024-10-28 15:37:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import pandas as pd
 st.title('🎈 Dashboard Beauty Product')
 
 st.write('This is interative dashboard beauty product')
-df = pd.read_csv('https://raw.githubusercontent.com/desims/Chat-AI/refs/heads/main/data/data2.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/desims/Chat-AI/refs/heads/main/data/data2.csv', delimiter=';')
 df
 
